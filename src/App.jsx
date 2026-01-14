a16855479fc07f4bbed8a1743a7b8bff4a577920
BFEmcompre <sac@emcompre.com.br>
2026-01-14 17:22:40 -0300

    correçao

--- src/App.jsx
+++ src/App.jsx
@@ -294,7 +294,7 @@ export default function App() {
   const res = data?.[0];
   if (res) {
     setMsg(
-      ``Compra registrada ✅ | Total ${brl(totalNow)} | Crédito usado ${brl(row?.credit_used || 0)}`
+      `Compra registrada ✅ | Total ${brl(totalNow)} | Crédito usado ${brl(row?.credit_used || 0)}`
     );
   }
 
